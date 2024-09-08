4dfbd11f2648ab9024cfe259e765af0b0e556e26
SNM1105 <simonmedalssy@gmail.com>
2024-09-08 16:56:54 -0400

    Added important comments to the blank page project

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,20 +1,30 @@
 /**
- * Function Calls
+ * The Blank Page
  * Simon Medalssy
  * 
- * Working on function calls examples.
+ * An exploration of the exestential angst of a novelist who must 
+ * sit down at a desk and confront the abyss that is a blank page of paper.
+ * 
+ * The program is non-interactive to convey the inability to get started on the project.
+ * Try typing! Just try it!
  */
 
 "use strict";
 
-
+/**
+ * Creates Canvas for our masterpiece
+ */
 function setup() {
-    //once at the beginning of ther program. Within the brackets are arguments thats define the width and height if the canvas.
+    // Create the canvas at a standard resolution
     createCanvas(640, 480);
 }
 
+/**
+ * Draws the writer's desktop and a blank piece of paper
+ */
 function draw() {
-    //every frame. Within parentheses are arguments that specify colour
+    //The pink/red desktop
     background(255, 100, 100);
+    //The blank piece of paper
     rect(200, 80, 240, 320);
 }
