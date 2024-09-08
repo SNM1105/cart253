f8fbb61e4b85b7238733f676919bc5e52992c329
SNM1105 <simonmedalssy@gmail.com>
2024-09-08 16:31:05 -0400

    Removed a file that was bugging me and created a red canvas

---
+++ topics/instructions/function-calls/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Function Calls
+ * Simon Medalssy
+ * 
+ * Working on function calls examples.
+ */
+
+"use strict";
+
+
+function setup() {
+    //once at the beginning of ther program. Within the brackets are arguments thats define the width and height if the canvas.
+    createCanvas(640, 480);
+}
+
+function draw() {
+    //every frame. Within parentheses are arguments that specify colour
+    background(255, 100, 100);
+}
