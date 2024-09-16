80d2f6081c7584fb1d446616b79d7fdc0f1627c1
SNM1105 <simonmedalssy@gmail.com>
2024-09-16 16:21:11 -0400

    Created a hole in a piece of cheese

---
+++ topics/variables/creating variables/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Creating Variables
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+const { Back } = require("gsap");
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(480,480)
+}
+
+
+/**
+ * Draws a hole in a piece of cheese
+*/
+function draw() {
+    // The cheese
+    background(255, 204, 0);
+
+    //The hole
+    push();
+    noStroke();
+    fill(0);
+    ellipse(140, 175, 100,100)
+}
