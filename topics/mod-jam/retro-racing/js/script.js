19fb77c8b8ae295a62a544fbb7019e4723f63a94
SNM1105 <simonmedalssy@gmail.com>
2024-10-22 15:22:25 -0400

    Started Pseudo Code for Retro Racing

--- topics/mod-jam/retro-racing/js/script.js
+++ topics/mod-jam/retro-racing/js/script.js
@@ -1,13 +1,15 @@
 /**
- * Frogfrogfrog
- * Pippin Barr
+ * Retro Racing
+ * Simon Medalssy
  * 
- * A game of catching flies with your frog-tongue
+ * A top down view driving game.
+ * Drive the car to the other end of the track. Careful not to hit the walls or you'll die!!!
  * 
  * Instructions:
- * - Move the frog with your mouse
- * - Click to launch the tongue
- * - Catch flies
+ * - navigate the car with your WASD keys
+ * - DON'T HIT THE WALLS OR ITS GAME OVER
+ * - Get to the other end of the track
+ * - Try again on a new track
  * 
  * Made with p5
  * https://p5js.org/
@@ -15,33 +17,11 @@
 
 "use strict";
 
-// Our frog
-const frog = {
-    // The frog's body has a position and size
-    body: {
-        x: 320,
-        y: 520,
-        size: 150
-    },
-    // The frog's tongue has a position, size, speed, and state
-    tongue: {
-        x: undefined,
-        y: 480,
-        size: 20,
-        speed: 20,
-        // Determines how the tongue moves each frame
-        state: "idle" // State can be: idle, outbound, inbound
-    }
+// The car
+const car = {
+
 };
 
-// Our fly
-// Has a position, size, and speed of horizontal movement
-const fly = {
-    x: 0,
-    y: 200, // Will be random
-    size: 10,
-    speed: 3
-};
 
 /**
  * Creates the canvas and initializes the fly
@@ -57,9 +37,9 @@ function draw() {
     background("#87ceeb");
     moveFly();
     drawFly();
-    moveFrog();
+    moveCar();
     moveTongue();
-    drawFrog();
+    drawCar();
     checkTongueFlyOverlap();
 }
 
@@ -77,17 +57,6 @@ function moveFly() {
 }
 
 /**
- * Draws the fly as a black circle
- */
-function drawFly() {
-    push();
-    noStroke();
-    fill("#000000");
-    ellipse(fly.x, fly.y, fly.size);
-    pop();
-}
-
-/**
  * Resets the fly to the left with a random y
  */
 function resetFly() {
@@ -98,85 +67,12 @@ function resetFly() {
 /**
  * Moves the frog to the mouse position on x
  */
-function moveFrog() {
-    frog.body.x = mouseX;
-}
-
-/**
- * Handles moving the tongue based on its state
- */
-function moveTongue() {
-    // Tongue matches the frog's x
-    frog.tongue.x = frog.body.x;
-    // If the tongue is idle, it doesn't do anything
-    if (frog.tongue.state === "idle") {
-        // Do nothing
-    }
-    // If the tongue is outbound, it moves up
-    else if (frog.tongue.state === "outbound") {
-        frog.tongue.y += -frog.tongue.speed;
-        // The tongue bounces back if it hits the top
-        if (frog.tongue.y <= 0) {
-            frog.tongue.state = "inbound";
-        }
-    }
-    // If the tongue is inbound, it moves down
-    else if (frog.tongue.state === "inbound") {
-        frog.tongue.y += frog.tongue.speed;
-        // The tongue stops if it hits the bottom
-        if (frog.tongue.y >= height) {
-            frog.tongue.state = "idle";
-        }
-    }
+function moveCar() {
+    car.body.x = mouseX;
 }
 
 /**
  * Displays the tongue (tip and line connection) and the frog (body)
  */
-function drawFrog() {
-    // Draw the tongue tip
-    push();
-    fill("#ff0000");
-    noStroke();
-    ellipse(frog.tongue.x, frog.tongue.y, frog.tongue.size);
-    pop();
-
-    // Draw the rest of the tongue
-    push();
-    stroke("#ff0000");
-    strokeWeight(frog.tongue.size);
-    line(frog.tongue.x, frog.tongue.y, frog.body.x, frog.body.y);
-    pop();
-
-    // Draw the frog's body
-    push();
-    fill("#00ff00");
-    noStroke();
-    ellipse(frog.body.x, frog.body.y, frog.body.size);
-    pop();
-}
-
-/**
- * Handles the tongue overlapping the fly
- */
-function checkTongueFlyOverlap() {
-    // Get distance from tongue to fly
-    const d = dist(frog.tongue.x, frog.tongue.y, fly.x, fly.y);
-    // Check if it's an overlap
-    const eaten = (d < frog.tongue.size/2 + fly.size/2);
-    if (eaten) {
-        // Reset the fly
-        resetFly();
-        // Bring back the tongue
-        frog.tongue.state = "inbound";
-    }
-}
-
-/**
- * Launch the tongue on click (if it's not launched yet)
- */
-function mousePressed() {
-    if (frog.tongue.state === "idle") {
-        frog.tongue.state = "outbound";
-    }
+function drawCar() {
 }
--- topics/mod-jam/retro-racing/pseudocode.md
+++ topics/mod-jam/retro-racing/pseudocode.md
@@ -1,72 +1,54 @@
-# Pseudocode for Frogfrogfrog
+# Pseudocode for Retro Racing
 
 ```
-frog
-    body
-        x: 320 // Halfway across a 640x480 canvas
-        y: 480 // Bottom of a 640x480 canvas
-        size: 100 // Diameter of the frog circle
-    tongue
-        x: undefined // Will always match the body
-        y: 480 // At the bottom (important to draw it BEHIND the frog)
-        size: 20 // The tip of the tongue
-        speed: 20 // Speed the tongue movies in pixels/second
-        state: idle // At the start the tongue hasn't been launched
+car {
+    x: 0 // starting at the left side of the screen
+    y: 240 // halfway down the canvas
+    size: 15 // small oval
+}
 
-fly
-    x: 0 // The left
-    y: 200? // This will be a random position...
-    size: 10 // Small?
-    speed: 3 // How fast it moves across the screen
+road {
+    somehow need to figure out how to randomly generate a map with all the segments connected together.
+    Will use Claude.AI or ChatGPT for assistance in learning.
+}
+
+offroad {
+    Everything that isn't the road will be offroad and will kill the player / game over
+}
+
+GameOver {
+    Draw a game over screen when the player dies
+}
 
 setup()
-    Create a 640x480 canvas
+    720 by 480 canvas. long map
 
 draw()
-    Draw the background // Probably just blue or something
-    moveFly()
-    drawFly()
-    moveFrog()
-    moveTongue()
-    drawFrog()
-    checkTongueFlyOverlap()
+    Background will be grassy green
+    drawCar
+    moveCar
+    drawRoad
+    drawOffroad
+    drawGameOver
 
-moveFly()
-    add fly speed to fly x
-    if (fly x is past the right side of the canvas)
-        move the fly back to the left
-        give the fly a random y position
+drawCar()
+    drawCarBody
+    drawCarMirrors
+    drawCarWindshield
+    drawCarHeadlights
 
-drawFly()
-    Draw a black circle at the fly's position with its size
+drawCarBody()
+    red rectangle w/ rounded corners
 
-moveFrog()
-    Set the frog's x to the mouse x
+drawCarMirrors()
+    2 red ovals on each side of the car, near the front
 
-moveTongue()
-    Set tongue x to frog x
-    if (tongue state is idle)
-        Do nothing
-    else if (tongue state is outbound)
-        move the tongue up by its speed
-        if (tongue hit the top)
-            set the tongue state to inbound
-    else if (tongue state is inbound)
-        move the tongue down by its speed
-        if (tongue hit the bottom)
-            set the tongue state to idle
+drawCarWindshield()
+    light blue quad shape, 1 in front and 1 in back
 
-drawFrog()
-    Draw a red circle at the tongue position with its size
-    Draw a red line from the tongue position to the frog position
-    Draw a green circle at the frog position with its size
+drawCarHeadlights()
+    2 yellow circles on the front of the car
 
-checkTongueFlyOverlap()
-    if (tongue circle overlaps the fly)
-        Move the fly back to the left at a random y
-        set the tongue state to inbound
+moveCar ()
 
-mousePressed()
-    if (tongue state is idle)
-        set tongue state to outbound
 ```
