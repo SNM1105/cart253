7553d31e959f68f9cedb2a1caebd0ec15c793643
SNM1105 <simonmedalssy@gmail.com>
2024-10-29 14:38:09 -0400

    Finishes Buzzy The Parameters

--- topics/functions/buzzy-the-return-value/buzzy-the-return-value/js/script.js
+++ topics/functions/buzzy-the-return-value/buzzy-the-return-value/js/script.js
@@ -1,26 +1,26 @@
 /**
- * Buzzy the parameter
+ * Buzzy the return value
  * Pippin Barr
  * 
- * A fly that buzzes around on the canvas
+ * Two flies that buzz around on the canvas
  */
 
 "use strict";
 
-// Our fly that will buzz around
+// Our flies that will buzz around
 let buzzyTheFly = {
     x: 200,
     y: 200,
-    size: 30,
-    buzziness: 4
+    size: 20,
+    buzziness: 3
 };
 
 let jazzyTheFly = {
-    x: 300,
-    y: 250,
-    size: 10,
-    buzziness: 7
-}
+    x: 200,
+    y: 200,
+    size: 20,
+    buzziness: 3
+};
 
 /**
  * Create a canvas
@@ -35,21 +35,24 @@ function setup() {
 function draw() {
     background("#87ceeb");
 
-    //Move buzzy
     moveFly(buzzyTheFly);
     moveFly(jazzyTheFly);
 
-    //Draw Buzzy
     drawFly(buzzyTheFly);
     drawFly(jazzyTheFly);
 }
 
-
+/**
+ * Move the fly passed in by updating its position
+ */
 function moveFly(fly) {
     fly.x += random(-fly.buzziness, fly.buzziness);
     fly.y += random(-fly.buzziness, fly.buzziness);
 }
 
+/**
+ * Draw the fly passed in using its properties
+ */
 function drawFly(fly) {
     push();
     noStroke();
